a40024a0674433d9ec65b7c06aa3bf01bdb871ad
Ian Zapolsky <ianzapolsky@gmail.com>
2014-09-29 20:59:12 -0400

    initial commit to ians branch. more work to come tonight

--- offset/wednesday/Player.java
+++ offset/wednesday/Player.java
@@ -1,4 +1,4 @@
-package offset.dumb;
+package offset.wednesday;
 
 import java.util.*;
 
@@ -22,52 +22,31 @@ public class Player extends offset.sim.Player {
 	public movePair move(Point[] grid, Pair pr, Pair pr0, ArrayList<ArrayList> history) {
 		//System.out.println(history.size());
 		movePair movepr = new movePair();
+
+    int p = pr.x;
+    int q = pr.y;
+
 		for (int i = 0; i < size; i++) {
+
 			for (int j = 0; j < size; j++) {
-				for (int i_pr=0; i_pr<size; i_pr++) {
-				for (int j_pr=0; j_pr <size; j_pr++) {
-					movepr.move = false;
-					movepr.x = grid[i*size+j];
-					movepr.y = grid[size*i_pr+j_pr];
-					if (validateMove(movepr, pr)) {
-						movepr.move = true;
-						return movepr;
-					}
+
+				for (int i_pr=0; i_pr < size; i_pr++) {
+
+				  for (int j_pr=0; j_pr < size; j_pr++) {
+
+				  	movepr.move = false;
+
+				  	movepr.x = grid[i * size + j];
+
+				  	movepr.y = grid[size * i_pr + j_pr];
+
+				  	if (validateMove(movepr, pr)) {
+				  		movepr.move = true;
+				  		return movepr;
+				  	}
+
+				  }
 				}
-				}
-			/*	if (i + pr.x >= 0 && i + pr.x < size) {
-					if (j + pr.y >= 0 && j + pr.y < size) {
-						
-					}
-					if (j - pr.y >= 0 && j - pr.y < size) {
-
-					}
-				}
-				if (i - pr.x >= 0 && i - pr.x < size) {
-					if (j + pr.y >= 0 && j + pr.y < size) {
-
-					}
-					if (j - pr.y >= 0 && j - pr.y < size) {
-
-					}
-				}
-				if (i + pr.y >= 0 && i + pr.y < size) {
-					if (j + pr.x >= 0 && j + pr.x < size) {
-
-					}
-					if (j - pr.x >= 0 && j - pr.x < size) {
-
-					}
-				}
-				if (i - pr.y >= 0 && i - pr.y < size) {
-					if (j + pr.x >= 0 && j + pr.x < size) {
-
-					}
-					if (j - pr.x >= 0 && j - pr.x < size) {
-
-					}
-				}
-*/
 			}
 		}
 		return movepr;
