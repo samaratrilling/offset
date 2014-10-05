74ca3cd08eafb812af66f2c6b90d370f7c53c8cf
Samara Trilling <samara.trilling@gmail.com>
2014-10-05 13:55:52 -0400

    Added two API methods to hello.py

--- MLapp/hello.py
+++ MLapp/hello.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/store', methods=['PUT'])
+def store_results():
+    return 'storing results'
+
+@app.route('/nextmove', methods=['GET'])
+def get_opponent_move():
+    return 'getting opponent next move'
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
