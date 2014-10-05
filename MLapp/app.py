8a3097d06cb313f80411c0f0f260632d044e87e0
Samara Trilling <samara.trilling@gmail.com>
2014-10-05 14:03:37 -0400

    Renamed app & added JSON error handler

--- MLapp/app.py
+++ MLapp/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import make_response
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+#Note: all return values should be in JSON format.
 @app.route('/store', methods=['PUT'])
 def store_results():
     return 'storing results'
@@ -13,6 +16,9 @@ def store_results():
 def get_opponent_move():
     return 'getting opponent next move'
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.debug = True
